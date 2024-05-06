d2a02ae2de6297ebb49d3746de312f262d36af69
mutthuappalaraju <arappalaraju@gmail.com>
2024-05-06 17:57:59 +0530

    tf-multi

---
+++ dev/backend.tf
@@ -0,0 +1,6 @@
+bucket = "mutthu2512-remote-dev"
+key    = "multienv"
+region = "us-east-1"
+dynamodb_table = "mutthu-dynamodb-dev"
+     
+        
---
+++ prod/backend.tf
@@ -0,0 +1,4 @@
+bucket = "mutthu2512-remote-prod"
+key    = "multienv"
+region = "us-east-1"
+dynamodb_table = "mutthu-dynamodb-prod"
